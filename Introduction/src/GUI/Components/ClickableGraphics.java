ac9d04c73f397b557d659db07c1c1d6b3a03e926
Student6 <Student6@10.8.33.166>
2016-12-22 13:15:14 -0500

    assignment 4

--- Introduction/src/GUI/Components/ClickableGraphics.java
+++ Introduction/src/GUI/Components/ClickableGraphics.java
@@ -1,5 +1,21 @@
 package GUI.Components;
 
-public class ClickableGraphics {
+public class ClickableGraphics  implements Clickable{
+
+	@Override
+	public boolean isHovered(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setAction(Action a)
+	{
+		
+	}
 
 }
